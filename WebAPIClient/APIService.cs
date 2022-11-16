9c44fd688578af1764480ab5ceb860e96ce2ecbc
Mateusz Ziemiewicz <matt.ziemiewicz@gmail.com>
2022-11-16 20:54:55 +0100

    More object oriented

---
+++ WebAPIClient/APIService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace GetTvShowTotalLength
+{
+    public class APIService
+    {
+        private readonly HttpClient _client;
+        public APIService(HttpClient client) { _client = client; }
+
+        public async Task<List<Show>> GetShowInfoByIdAsync(int id)
+        {
+            await using Stream stream =
+                await _client.GetStreamAsync($"https://api.tvmaze.com/shows/{id}/episodes");
+            var episodes =
+                await JsonSerializer.DeserializeAsync<List<Show>>(stream);
+
+            return episodes ?? new();
+        }
+
+        public async Task<int> GetShowIdAsync(string query)
+        {
+            int id = 0;
+            using HttpResponseMessage response =
+                await _client.GetAsync($"https://api.tvmaze.com/singlesearch/shows?q={query}");
+            if (response.StatusCode == HttpStatusCode.NotFound)
+                Environment.Exit(10);
+
+            try
+            {
+                await using Stream stream =
+                await _client.GetStreamAsync($"https://api.tvmaze.com/singlesearch/shows?q={query}");
+                var show =
+                    await JsonSerializer.DeserializeAsync<Show>(stream);
+                id = show.Id;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Environment.Exit(1);
+            }
+
+            return id;
+        }
+    }
+}
--- WebAPIClient/Program.cs
+++ WebAPIClient/Program.cs
@@ -1,54 +1,31 @@
-﻿using System.Text.Json;
-using GetTvShowTotalLength;
+﻿using GetTvShowTotalLength;
 using System.Net.Http.Headers;
 
-using HttpClient client = new();
-client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(
-    new MediaTypeWithQualityHeaderValue("application/json"));
-
-List<Show> episodes = await ProcessShowsAsync(client);
-int sumRuntime = 0;
-
-foreach (var episode in episodes ?? Enumerable.Empty<Show>())
+class Program
 {
-    sumRuntime += episode.Runtime ?? 0;
+    private static async Task<int> Main(string[] args)
+    {
+        if (args == null || args.Length == 0)
+        {
+            Environment.Exit(1);
+        }
+        string query = Convert.ToString(args[0]);
+        
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        APIService service = new(client);
+
+        int id = service.GetShowIdAsync(query).Result;
+        List<Show> episodes = await service.GetShowInfoByIdAsync(id);
+
+        int sumRuntime = 0;
+        foreach (var episode in episodes ?? Enumerable.Empty<Show>())
+        {
+            sumRuntime += episode.Runtime ?? 0;
+        }
+        Console.Write(sumRuntime);
+        return 0;
+    }
 }
-
-
-async Task<List<Show>> ProcessShowsAsync(HttpClient client)
-{
-    if (args == null || args.Length == 0)
-    {
-        Environment.Exit(1);
-    }
-
-    string query = Convert.ToString(args[0]);
-    int id = 0;
-
-    // Try to call api to find id of a show
-    try
-    {
-        await using Stream streamId =
-            await client.GetStreamAsync($"https://api.tvmaze.com/singlesearch/shows?q={query}");
-
-        var queriedShow =
-            await JsonSerializer.DeserializeAsync<Show>(streamId);
-        id = queriedShow.Id;
-    }
-    catch (HttpRequestException)
-    {
-        Environment.Exit(10);
-    }
-
-    // Call api for a list of episodes (using the obtained id)
-    await using Stream stream =
-        await client.GetStreamAsync($"https://api.tvmaze.com/shows/{id}/episodes");
-    var episodes =
-        await JsonSerializer.DeserializeAsync<List<Show>>(stream);
-
-    return episodes ?? new();
-}
-
-Console.Write(sumRuntime);
-return 0;
