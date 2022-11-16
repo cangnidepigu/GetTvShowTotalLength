3ff10ec6d4a4fa187263cbb0f5b2d4db38f5be6c
Mateusz Ziemiewicz <matt.ziemiewicz@gmail.com>
2022-11-16 22:23:28 +0100

    Change from multiprocessing to threading

--- tv-time.py
+++ tv-time.py
@@ -10,7 +10,11 @@ On start, calls to GetTvShowTotalLength in parallel, filters out None values and
 import os
 import subprocess
 import sys
-from multiprocessing import Pool
+# from multiprocessing import Pool
+import threading
+import queue
+
+MAX_THREADS = 5
 
 try:
     BINARY = os.environ.get('GET_TVSHOW_TOTAL_LENGTH_BIN').replace('\"', '')
@@ -19,7 +23,9 @@ except:
     sys.exit()
 
 show_list = []
+result_queue = queue.Queue()
 clean_result = {}
+semapho = threading.Semaphore(MAX_THREADS)
 
 # Process the input file
 input = sys.stdin.readlines()
@@ -28,6 +34,7 @@ for line in input:
 
 # Returns title of a tv-show and total runtime in minutes
 def GetTvShowTotalLength(title):
+    semapho.acquire()
     process = subprocess.Popen([fr'{BINARY}', '\"{q}\"'.format(q=title)], stdout=subprocess.PIPE, text=True)
     stdout, stderr = process.communicate()
     if stdout.isnumeric():
@@ -35,17 +42,28 @@ def GetTvShowTotalLength(title):
     exit_code = process.wait()
     
     if exit_code != 10:
-        return {title: stdout}
+        result_queue.put({title: stdout})
+        semapho.release()
+        # return {title: stdout}
     else:
         print('Could not get info for {q}.'.format(q=title), file=sys.stderr)
+        semapho.release()
 
 if __name__ == '__main__':
-    # Run in parallel
-    with Pool() as pool:
-        raw_result = pool.map(GetTvShowTotalLength, [title for title in show_list])
-    result = list(filter(lambda item: item is not None, raw_result))
+    threads = []
+    for title in show_list:
+        t = threading.Thread(target=GetTvShowTotalLength, args=(title,))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+
+    result = []
+    while not result_queue.empty():
+        result.append(result_queue.get())
     
     for item in result:
+        print(item)
         clean_result.update(item)
 
     longest = max(clean_result.items(), key=lambda k: k[1])
@@ -53,3 +71,16 @@ if __name__ == '__main__':
 
     print('The shortest show: {t} ({h}h {m}m)'.format(t=shortest[0], h=int(shortest[1])//60, m=int(shortest[1])%60))
     print('The longest show: {t} ({h}h {m}m)'.format(t=longest[0], h=int(longest[1])//60, m=int(longest[1])%60))
+    # Run in parallel
+    # with Pool() as pool:
+    #     raw_result = pool.map(GetTvShowTotalLength, [title for title in show_list])
+    # result = list(filter(lambda item: item is not None, raw_result))
+    
+    # for item in result:
+    #     clean_result.update(item)
+
+    # longest = max(clean_result.items(), key=lambda k: k[1])
+    # shortest = min(clean_result.items(), key=lambda k: k[1])
+
+    # print('The shortest show: {t} ({h}h {m}m)'.format(t=shortest[0], h=int(shortest[1])//60, m=int(shortest[1])%60))
+    # print('The longest show: {t} ({h}h {m}m)'.format(t=longest[0], h=int(longest[1])//60, m=int(longest[1])%60))
